be911f748d0c2bd9ef4a49e97a6184bfc8db40c2
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-28 15:02:07 +0100

    [ADD] options displaying function in english

---
+++ src/en/display_options.py
@@ -0,0 +1,11 @@
+from colorama import Fore
+
+def display_options():
+
+    print(Fore.BLUE + "\nAvailable options :\n")
+    print(Fore.BLUE + "1. \"list\" - List tasks")
+    print(Fore.BLUE + "2. \"add\" - Add a task")
+    print(Fore.BLUE + "3. \"rm\" - Remove a task")
+    print(Fore.BLUE + "4. \"mod\"- Modify a task")
+    print(Fore.BLUE + "5. \"help\" - Display available options at any time")
+    print(Fore.BLUE + "6. \"exit\" - Exit program")
