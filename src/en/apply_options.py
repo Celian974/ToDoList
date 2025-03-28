49dab8eafecc7c81d1033fe69c5e926214f88714
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-28 18:36:36 +0100

    [FIX] improve error_handling

--- src/en/apply_options.py
+++ src/en/apply_options.py
@@ -36,24 +36,31 @@ def apply_options():
         elif choice == "rm":
 
             if tasks:
-                task_id = int(input(Fore.MAGENTA + "\nEnter the ID of the task to remove : ").strip())
+                while True:
+                    try:
+                        task_id = int(input(Fore.MAGENTA + "\nEnter the ID of the task to remove : ").strip())
 
-                if 0 < task_id <= len(tasks):
-                    confirmation = input(Fore.YELLOW + "Are you sure that you want to remove this task ? [yes/no] : ")
+                        if 0 < task_id <= len(tasks):
+                            confirmation = input(Fore.YELLOW + "Are you sure that you want to remove this task ? [yes/no] : ")
 
-                    if confirmation == "yes":
-                        removed_task = tasks.pop(task_id - 1)
-                        save_tasks(tasks)
-                        print(Fore.GREEN + f"Task \"{removed_task}\" has been successfully removed.")
+                            if confirmation == "yes":
+                                removed_task = tasks.pop(task_id - 1)
+                                save_tasks(tasks)
+                                print(Fore.GREEN + f"Task \"{removed_task}\" has been successfully removed.")
+                                break
 
-                    elif confirmation == "no":
-                        print(Fore.YELLOW + "Aborting...")
+                            elif confirmation == "no":
+                                print(Fore.YELLOW + "Aborting...")
+                                break
 
-                    else:
-                        print(Fore.RED + "Incorrect choice. Aborting...")
+                            else:
+                                print(Fore.RED + "Incorrect choice. Aborting...")
 
-                else:
-                    print(Fore.RED + "\nThis ID is incorrect.")
+                        else:
+                            print(Fore.RED + "\nThis ID is incorrect.")
+
+                    except ValueError:
+                        print(Fore.RED + "\nInvalid input. Please enter a valid task ID (number).")
 
             else:
                 print(Fore.RED + "\nThere are currently no tasks added.")
@@ -61,22 +68,31 @@ def apply_options():
         elif choice == "mod":
 
             if tasks:
-                task_id = int(input(Fore.MAGENTA + "\nEnter the ID of the task to modify : ").strip())
+                while True:
+                    try:
+                        task_id = int(input(Fore.MAGENTA + "\nEnter the ID of the task to modify : ").strip())
 
-                if 0 < task_id <= len(tasks):
-                    modified_task = input(Fore.MAGENTA + "\nEnter the new task description : ")
-                    confirmation = input(Fore.YELLOW + "Are you sure that you want to modify this task ? [yes/no] : ")
+                        if 0 < task_id <= len(tasks):
+                            modified_task = input(Fore.MAGENTA + "\nEnter the new task description : ")
+                            confirmation = input(Fore.YELLOW + "Are you sure that you want to modify this task ? [yes/no] : ")
 
-                    if confirmation == "yes":
-                        print(Fore.GREEN + f"Task \"{tasks[task_id - 1]}\" has been successfully updated to \"{modified_task}\".")
-                        tasks[task_id - 1] = modified_task
-                        save_tasks(tasks)
+                            if confirmation == "yes":
+                                tasks[task_id - 1] = modified_task
+                                save_tasks(tasks)
+                                print(Fore.GREEN + f"Task \"{tasks[task_id - 1]}\" has been successfully updated to \"{modified_task}\".")
+                                break
 
-                    elif confirmation == "no":
-                        print(Fore.YELLOW + "Aborting...")
+                            elif confirmation == "no":
+                                print(Fore.YELLOW + "Aborting...")
+                                break
 
-                    else:
-                        print(Fore.RED + "Incorrect choice. Aborting...")
+                            else:
+                                print(Fore.RED + "Incorrect choice. Aborting...")
+                        else:
+                            print(Fore.RED + "\nThis ID is incorrect.")
+
+                    except ValueError:
+                        print(Fore.RED + "\nInvalid input. Please enter a valid task ID (number).")
 
             else:
                 print(Fore.RED + "\nThere are currently no tasks added.")
