320c3908eb5c3f1691ce83404dbcacc81353e2df
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-28 15:00:29 +0100

    [ADD] options displaying function in french

---
+++ src/fr/display_options.py
@@ -0,0 +1,11 @@
+from colorama import Fore
+
+def display_options():
+
+    print(Fore.BLUE + "\nOptions disponibles  :\n")
+    print(Fore.BLUE + "1. \"list\" - Lister les tâches")
+    print(Fore.BLUE + "2. \"add\" - Ajouter une tâche")
+    print(Fore.BLUE + "3. \"rm\" - Supprimer une tâche")
+    print(Fore.BLUE + "4. \"mod\" - Modifier une tâche")
+    print(Fore.BLUE + "5. \"help\" - Afficher les options disponibles à tout moment")
+    print(Fore.BLUE + "6. \"exit\" - Quitter le programme")
