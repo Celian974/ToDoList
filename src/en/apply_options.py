59e9f9cda76a6b5787029ea7457fcaacb6a2f6c6
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-28 15:02:22 +0100

    [ADD] options applying function in english

---
+++ src/en/apply_options.py
@@ -0,0 +1,92 @@
+from colorama import Fore
+from display_options import display_options
+from load_tasks import load_tasks
+from save_tasks import save_tasks
+
+def apply_options():
+
+    tasks = load_tasks()
+    options = ["add", "rm", "list", "mod", "exit", "help"]
+
+    print(Fore.WHITE + "\nWelcome to To-Do List program !")
+
+    display_options()
+
+    while True:
+        choice = input(Fore.MAGENTA + "\nSelect an option : ").strip()
+
+        if choice in options:
+            print(Fore.GREEN + f"Selected option : {choice}")
+
+        if choice == "list":
+
+            if tasks:
+                print(Fore.CYAN + "\nAdded tasks : \n")
+                for i, task in enumerate(tasks, 1):
+                    print(f"{i}. {task}")
+            else:
+                print(Fore.RED + "\nThere are currently no tasks added.")
+
+        elif choice == "add":
+            task = input(Fore.MAGENTA + "\nEnter a new task : ").strip()
+            tasks.append(task)
+            save_tasks(tasks)
+            print(Fore.GREEN + f"Task \"{task}\" has been successfully added.")
+
+        elif choice == "rm":
+
+            if tasks:
+                task_id = int(input(Fore.MAGENTA + "\nEnter the ID of the task to remove : ").strip())
+
+                if 0 < task_id <= len(tasks):
+                    confirmation = input(Fore.YELLOW + "Are you sure that you want to remove this task ? [yes/no] : ")
+
+                    if confirmation == "yes":
+                        removed_task = tasks.pop(task_id - 1)
+                        save_tasks(tasks)
+                        print(Fore.GREEN + f"Task \"{removed_task}\" has been successfully removed.")
+
+                    elif confirmation == "no":
+                        print(Fore.YELLOW + "Aborting...")
+
+                    else:
+                        print(Fore.RED + "Incorrect choice. Aborting...")
+
+                else:
+                    print(Fore.RED + "\nThis ID is incorrect.")
+
+            else:
+                print(Fore.RED + "\nThere are currently no tasks added.")
+
+        elif choice == "mod":
+
+            if tasks:
+                task_id = int(input(Fore.MAGENTA + "\nEnter the ID of the task to modify : ").strip())
+
+                if 0 < task_id <= len(tasks):
+                    modified_task = input(Fore.MAGENTA + "\nEnter the new task description : ")
+                    confirmation = input(Fore.YELLOW + "Are you sure that you want to modify this task ? [yes/no] : ")
+
+                    if confirmation == "yes":
+                        print(Fore.GREEN + f"Task \"{tasks[task_id - 1]}\" has been successfully updated to \"{modified_task}\".")
+                        tasks[task_id - 1] = modified_task
+                        save_tasks(tasks)
+
+                    elif confirmation == "no":
+                        print(Fore.YELLOW + "Aborting...")
+
+                    else:
+                        print(Fore.RED + "Incorrect choice. Aborting...")
+
+            else:
+                print(Fore.RED + "\nThere are currently no tasks added.")
+
+        elif choice == "help":
+            display_options()
+
+        elif choice == "exit":
+            print(Fore.BLACK + "\nExiting program... Goodbye !\n")
+            break
+
+        else:
+            print(Fore.RED + "\nThis option is incorrect.")
