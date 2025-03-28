0b1c3450b6cc3af0dba8a167a4dff440e7c2bbf7
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-28 15:02:37 +0100

    [ADD] task loading function in english

---
+++ src/en/load_tasks.py
@@ -0,0 +1,11 @@
+import json
+
+def load_tasks():
+    try:
+        with open("tasks.json", "r") as file:
+            tasks = json.load(file)
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        tasks = []
+
+    return tasks
