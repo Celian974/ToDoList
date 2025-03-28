caf822e6c1de018265f6da0d440dd3e657a9e80d
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-28 15:30:19 +0100

    [FIX] help option text fix

--- src/fr/display_options.py
+++ src/fr/display_options.py
@@ -7,5 +7,5 @@ def display_options():
     print(Fore.BLUE + "2. \"add\" - Ajouter une tâche")
     print(Fore.BLUE + "3. \"rm\" - Supprimer une tâche")
     print(Fore.BLUE + "4. \"mod\" - Modifier une tâche")
-    print(Fore.BLUE + "5. \"help\" - Afficher les options disponibles à tout moment")
+    print(Fore.BLUE + "5. \"help\" - Afficher les options disponibles")
     print(Fore.BLUE + "6. \"exit\" - Quitter le programme")
