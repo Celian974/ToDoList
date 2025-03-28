6cfc1c5722149eb0b2d6599018a0901ae43f59bd
Celian Siampirave <celian.siampirave@epitech.eu>
2025-03-28 19:11:48 +0100

    [FIX] mod option success message

--- src/fr/apply_options.py
+++ src/fr/apply_options.py
@@ -37,24 +37,29 @@ def apply_options():
         elif choice == "rm":
 
             if tasks:
-                task_id = int(input(Fore.MAGENTA + "\nEntrez l'identifiant de la tâche à supprimer : ").strip())
+                while True:
+                    try:
+                        task_id = int(input(Fore.MAGENTA + "\nEntrez l'identifiant de la tâche à supprimer : ").strip())
 
-                if 0 < task_id <= len(tasks):
-                    confirmation = input(Fore.YELLOW + "Êtes-vous sûr de vouloir supprimer cette tâche ? [oui/non] : ")
+                        if 0 < task_id <= len(tasks):
+                            confirmation = input(Fore.YELLOW + "Êtes-vous sûr de vouloir supprimer cette tâche ? [oui/non] : ")
 
-                    if confirmation == "oui":
-                        removed_task = tasks.pop(task_id - 1)
-                        save_tasks(tasks)
-                        print(Fore.GREEN + f"La tâche \"{removed_task}\" a été supprimée avec succès.")
+                            if confirmation == "oui":
+                                removed_task = tasks.pop(task_id - 1)
+                                save_tasks(tasks)
+                                print(Fore.GREEN + f"La tâche \"{removed_task}\" a été supprimée avec succès.")
 
-                    elif confirmation == "non":
-                        print(Fore.YELLOW + "Annulation...")
+                            elif confirmation == "non":
+                                print(Fore.YELLOW + "Annulation...")
 
-                    else:
-                        print(Fore.RED + "Choix incorrect. Annulation...")
+                            else:
+                                print(Fore.RED + "Choix incorrect. Annulation...")
 
-                else:
-                    print(Fore.RED + "Cet identifiant est incorrect.")
+                        else:
+                            print(Fore.RED + "Cet identifiant est incorrect.")
+
+                    except ValueError:
+                        print(Fore.RED + "\nEntrée incorrecte. Veuillez entrer un identifiant de tâche valide (nombre).")
 
             else:
                 print(Fore.RED + "\nIl n'y a actuellement aucune tâche ajoutée.")
@@ -62,22 +67,30 @@ def apply_options():
         elif choice == "mod":
 
             if tasks:
-                task_id = int(input(Fore.MAGENTA + "\nEntrez l'identifiant de la tâche à modifier : ").strip())
+                while True:
+                    try:
+                        task_id = int(input(Fore.MAGENTA + "\nEntrez l'identifiant de la tâche à modifier : ").strip())
 
-                if 0 < task_id <= len(tasks):
-                    modified_task = input(Fore.MAGENTA + "\nEntrez la nouvelle description de la tâche: ")
-                    confirmation = input(Fore.YELLOW + "Êtes-vous sûr de vouloir modifier cette tâche ? [oui/non] : ")
+                        if 0 < task_id <= len(tasks):
+                            modified_task = input(Fore.MAGENTA + "\nEntrez la nouvelle description de la tâche: ")
+                            confirmation = input(Fore.YELLOW + "Êtes-vous sûr de vouloir modifier cette tâche ? [oui/non] : ")
 
-                    if confirmation == "oui":
-                        print(Fore.GREEN + f"Task \"{tasks[task_id - 1]}\" a été modifiée en \"{modified_task}\" avec succès.")
-                        tasks[task_id - 1] = modified_task
-                        save_tasks(tasks)
+                            if confirmation == "oui":
+                                print(Fore.GREEN + f"Task \"{tasks[task_id - 1]}\" a été modifiée en \"{modified_task}\" avec succès.")
+                                tasks[task_id - 1] = modified_task
+                                save_tasks(tasks)
+                                break
+                            elif confirmation == "non":
+                                print(Fore.YELLOW + "Annulation...")
 
-                    elif confirmation == "non":
-                        print(Fore.YELLOW + "Annulation...")
+                            else:
+                                print(Fore.RED + "Choix incorrect. Annulation...")
 
-                    else:
-                        print(Fore.RED + "Choix incorrect. Annulation...")
+                        else:
+                            print(Fore.RED + "Cet identifiant est incorrect.")
+
+                    except ValueError:
+                        print(Fore.RED + "\nEntrée incorrecte. Veuillez entrer un identifiant de tâche valide (nombre).")
 
             else:
                 print(Fore.RED + "\nIl n'y a actuellement aucune tâche ajoutée.")
